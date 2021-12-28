f989cdf87b2dd92f7c190244b9b3bbb4e3c17998
nghho <nghho@VN-188.jonckers.be>
2021-12-28 16:29:24 +0700

    first comit

---
+++ testcases/facebook/web/page/Page_01_Create_Page.java
@@ -0,0 +1,19 @@
+package facebook.web.page;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Page_01_Create_Page {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
